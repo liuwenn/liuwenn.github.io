2197aac9ee29df9500bf4201fdc76a62ce8ce415
lw <441567056@qq.com>
2020-12-18 18:37:22 +0800

    11

--- app/html5-canvas/js/index.js
+++ app/html5-canvas/js/index.js
@@ -24,7 +24,7 @@ var S = {
     if (i !== -1) {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
-      S.UI.simulate('Shape|Shifter|Type|to start|#rectangle|#countdown 3||');
+      S.UI.simulate('爱|你|哟|#rectangle|#countdown 3||');
     }
 
     S.Drawing.loop(function () {
